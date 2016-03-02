ecd97c1af623909a252cb236cc260e44b2e6f97f
Sravanthi Rani Sinha <37@p6-2.hbtn.io>
2016-03-01 19:25:28 -0800

    finished JS task 0

--- post_a_status.js
+++ post_a_status.js
@@ -2,6 +2,6 @@ window.onload = function(){
 var el = document.getElementById('poststatus');
 el.onclick = function () {
   /*  document.getElementById('poststatus').style.visibility='hidden';*/
-  document.getElementById('postform').style.visibility='visible';
+  document.getElementById('postform').style.display='flex';
 }
 }
