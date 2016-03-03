01c4af7e3c23f5fb6a465cf879f5f2639a666a76
Sravanthi Rani Sinha <37@p6-2.hbtn.io>
2016-03-02 16:24:43 -0800

    finshed js task 1

--- post_a_status.js
+++ post_a_status.js
@@ -1,7 +1,7 @@
-window.onload = function(){
-var el = document.getElementById('poststatus');
-el.onclick = function () {
-  /*  document.getElementById('poststatus').style.visibility='hidden';*/
-  document.getElementById('postform').style.display='flex';
-}
-}
+document.addEventListener("DOMContentLoaded", function () {
+    var el = document.getElementById('poststatus');
+    el.onclick = function () {
+        /*  document.getElementById('poststatus').style.visibility='hidden';*/
+        document.getElementById('postform').style.display = 'flex';
+    };
+}, false);
