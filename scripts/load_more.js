3b400e5d9b92ac26017595e35880fd06e9b2837d
Sravanthi Rani Sinha <37@p6-2.hbtn.io>
2016-03-22 14:22:22 -0700

    finsiged task :Factorize

--- scripts/load_more.js
+++ scripts/load_more.js
@@ -1,23 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('showmoreButton').addEventListener('click', function loadDoc() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (xhttp.readyState == 4 && xhttp.status == 200) {
-                var a = document.getElementById("showmore");
-                a.style.display = 'none';
-                document.getElementById("morestatuses").innerHTML = xhttp.responseText;
-                var elements = document.querySelectorAll('.replyAll');
+        ajaxGet('/statuses-1.html', function showMoreResponses(response) {
+            var a = document.getElementById("showmore");
+            a.style.display = 'none';
+            document.getElementById("morestatuses").innerHTML = response;
+            var elements = document.querySelectorAll('.replyAll');
 
-                for (var i = 0; i < elements.length; i++) {
-                    var element = elements[i];
-                    element.onclick = function () {
-                        var form = document.getElementById(this.dataset.replyindex);
-                        form.toggle();
-                    };
-                }
+            for (var i = 0; i < elements.length; i++) {
+                var element = elements[i];
+                element.onclick = function () {
+                    var form = document.getElementById(this.dataset.replyindex);
+                    form.toggle();
+                };
             }
-        };
-        xhttp.open("GET", "/statuses-1.html", true);
-        xhttp.send();
+        });
     });
 });
